37594c5d2900defa685fd2c4980f0ddbc8558846
delugegt <delugegt@hotmail.com>
2021-09-16 11:19:19 -0300

    commit sexto, eat food

--- script.js
+++ script.js
@@ -63,9 +63,16 @@ function iniciarJogo () {
   if (direction == "left")  snakeX -= box;
   if (direction == "up") snakeY -= box;
   if (direction == "down") snakeY += box; 
+//
+  if (snakeX != food.x || snakeY != food.y) {
+    snake.pop();
+  } else {
+    food.x = Math.floor(Math.random() * 15 + 1) * box;
+    food.y = Math.floor(Math.random() * 15 + 1) * box;
+  }
 
   //função que irá retirar o ultimo elemento do arrya
-  snake.pop();
+  //snake.pop();
 
   let newHead = {
     x : snakeX,
