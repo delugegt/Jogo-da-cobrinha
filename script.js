f8d62372f516f4d8a71be5e5f701dc273dd370d8
delugegt <delugegt@hotmail.com>
2021-09-16 10:22:25 -0300

    commit terciario HEADSnake

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ snake[0] = { //detalhando indices/itens do array
   x: 8 * box,
   y: 8 * box
 }
+let direction = "right";
+
 function criarBG() {
   context.fillStyle = "lightgreen"; //aplica cor ao  context
   context.fillRect(0, 0, 16 * box, 16 * box); //tamanho de 16 px
@@ -18,5 +20,30 @@ function criarCobrinha() {
   }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo () {
+  criarBG();
+  criarCobrinha();
+
+  //definir ponto de partida
+  //a posição de snake na posição X,Y
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y; 
+
+  //se direction for igual a right, acrescenta um box na snakeX
+  if (direction == "right") snakeX += box;
+  if (direction == "left")  snakeX -= box;
+  if (direction == "up") snakeY -= box;
+  if (direction == "down") snakeY += box; 
+
+  //função que irá retirar o ultimo elemento do arrya
+  snake.pop();
+
+  let newHead = {
+    x = snakeX,
+    y = snakeY
+  }
+
+  snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); //intervalo de 100 milisec para iniciar o jogo, renovar e dar continuidade ao jogo se travar 
